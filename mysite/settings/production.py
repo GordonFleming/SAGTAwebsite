34d841b965d84b5158402ce2f28d8daca72dbdf6
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-04-16 19:09:33 +0700

    Add Rancher deployment guide

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -7,7 +7,8 @@ WAGTAIL_SITE_NAME = os.environ.get('SITE_NAME')
 HTTP_PORT = os.environ.get('HTTP_PORT')
 ALLOWED_HOSTS = [
     '{}'.format(WAGTAIL_SITE_NAME), 'www.{}'.format(WAGTAIL_SITE_NAME)]
-
+DATABASES['default']['NAME'] = os.environ.get(
+    'SQLITE_LOCATION', DATABASES['default']['NAME'])
 try:
     from .local import *
 except ImportError:
