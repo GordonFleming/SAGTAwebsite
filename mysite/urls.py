5b98329fb6c5c8831350a4909bf560100913743c
William Moolman <william.moolman@hackpartners.com>
2020-04-23 11:15:19 +0100

    Refactored urls.py for import grouping

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.urls import path
+import debug_toolbar
 
+from django.conf import settings
+from django.conf.urls import include, url, static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -23,14 +25,11 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     #Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
