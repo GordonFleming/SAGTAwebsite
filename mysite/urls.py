51049e16a54a286eddd6db6bae8a497ac25c92ed
William Moolman <william.moolman@hackpartners.com>
2020-04-23 11:46:32 +0100

    Corrected static syntax

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,7 +28,7 @@ if settings.DEBUG:
 
     #Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
