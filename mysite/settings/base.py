ae2227c48909821d2b032ec1bbda03fb1b28600d
Gordon <flemingrgordon@gmail.com>
2020-04-29 15:01:57 +0200

    fix

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -198,3 +198,20 @@ ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_PRESERVE_USERNAME_CASING = False
 ACCOUNT_SESSION_REMEMBER = True
+
+
+# Don't download files hook
+
+from django.http import HttpResponse
+from wagtail.core import hooks
+from django.shortcuts import redirect
+
+@hooks.register('before_serve_document')
+def serve_pdf(document, request):
+    if document.file_extension != 'pdf':
+        return  # Empty return results in the existing response
+    response = HttpResponse(document.file.read(), content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="' + document.file.name.split('/')[-1] + '"'
+    if request.GET.get('download', False) in [True, 'True', 'true']:
+        response['Content-Disposition'] = 'attachment; ' + response['Content-Disposition']
+    return response
