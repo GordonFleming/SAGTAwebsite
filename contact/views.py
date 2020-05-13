65dd12e07098ff1aa76238143f541ba8c641a590
Gordon <flemingrgordon@gmail.com>
2020-05-13 21:05:42 +0200

    Contact form fix email

--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+def index(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        
+        send_mail('Contact Form',
+            message,
+            settings.EMAIL_HOST_USER,
+            'flemingrgordon@gmail.com',
+            fail_silently=False)
+        return render(request, 'contact/contact_page.html')
