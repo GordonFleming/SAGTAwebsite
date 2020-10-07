e4a09c5f10e7956792a0370cb81970b9c62074cb
Gordon <flemingrgordon@gmail.com>
2020-10-07 19:12:00 +0200

    Style changes and table added

--- mysite/static/js/mysite.js
+++ mysite/static/js/mysite.js
@@ -1,3 +1,8 @@
 $('.carousel').carousel({
     interval: 6000   
-  })
+})
+
+var apply = document.getElementById("applyNav");
+apply.addEventListener("click",function(){
+    document.getElementById('Apply').scrollIntoView({ behavior: 'smooth', block: 'center' });
+})
