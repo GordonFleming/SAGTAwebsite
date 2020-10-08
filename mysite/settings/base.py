9828ef056b46b6c9b3a2aa52148b2feae67b325e
Gordon <flemingrgordon@gmail.com>
2020-10-08 12:22:39 +0200

    Attempt to fix double login

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -214,3 +214,4 @@ DEFAULT_FROM_EMAIL = 'SAGTA Team <website@sagta.org.za>'
 
 #Set Wagtail default login to site design
 WAGTAIL_FRONTEND_LOGIN_TEMPLATE = 'account/login.html'
+WAGTAIL_FRONTEND_LOGIN_URL = '/accounts/login/'
