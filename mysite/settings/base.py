606c09f53a4437802c98c72f497f4d7138309ef2
Gordon Fleming <flemingrgordon@gmail.com>
2022-06-22 11:34:09 +0200

    upgrade to wagtail v3

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -210,7 +210,7 @@ WAGTAIL_SITE_NAME = "SAGTA"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'https://sagta.org.za'
+WAGTAILADMIN_BASE_URL = 'https://sagta.org.za'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
