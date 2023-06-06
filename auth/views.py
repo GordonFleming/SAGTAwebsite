76310969185dcc0d9204de06a96805bf83da4528
dimasciput <dimas.ciputra@gmail.com>
2023-06-06 16:57:03 +0700

    Logout should also removing the access and refresh token

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from allauth.account.views import LogoutView
+from oauth2_provider.models import AccessToken, RefreshToken
+
+
+class CustomLogoutView(LogoutView):
+    def logout(self):
+        AccessToken.objects.filter(user=self.request.user).delete()
+        RefreshToken.objects.filter(user=self.request.user).delete()
+        super().logout()
