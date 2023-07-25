72054d86af5d3d3aaa8cfa03437f98a8bffe2827
Gordon Fleming <flemingrgordon@gmail.com>
2023-07-25 13:46:52 +0200

    minor improvements

--- auth/forms.py
+++ auth/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
 class SignupForm(forms.Form):
-    first_name = forms.CharField(max_length=30, label='First Name')
-    last_name = forms.CharField(max_length=30, label='Last Name')
+    first_name = forms.CharField(max_length=30, label='First Name', 
+                                 widget=forms.TextInput(attrs={'placeholder':'First Name'}))
+    last_name = forms.CharField(max_length=30, label='Last Name',
+                                 widget=forms.TextInput(attrs={'placeholder':'Last Name'}))
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
