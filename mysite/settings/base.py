1083ba9767e73e7d5be323cba47a3fab8d458b09
Gordon Fleming <flemingrgordon@gmail.com>
2023-10-03 19:08:28 +0200

    Update base.py

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -227,14 +227,14 @@ ACCOUNT_SIGNUP_FORM_CLASS = 'auth.forms.SignupForm'
 ACCOUNT_ADAPTER =  'auth.adapter.RestrictEmailAdapter'
 
 # Email Settings
-#EMAIL_HOST = ''
-#EMAIL_HOST_USER = ''
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#DEFAULT_FROM_EMAIL = 'SAGTA Team <>'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'SAGTA Team <sagta@splyce.dev>'
 #TO_EMAIL = ''
 
 #Set Wagtail default login to site design
