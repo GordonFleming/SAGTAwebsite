749c3117bc89ba79f4dace5a7247a12e57a573c7
Gordon Fleming <flemingrgordon@gmail.com>
2024-03-20 13:42:13 +0200

    wagtail menus implemented

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
     'turnstile',
     'wagtail.api.v2',
+    'wagtailmenus',
 ]
 
 OAUTH2_PROVIDER = {
@@ -124,6 +125,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'wagtail.contrib.settings.context_processors.settings',
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
