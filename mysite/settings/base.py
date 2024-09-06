af3ebd1ea5ec8f3677c09b419a31dd8ecebda68b
Gordon Fleming <flemingrgordon@gmail.com>
2024-09-06 22:35:01 +0100

    revert oauth version

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -212,8 +212,6 @@ MEDIA_URL = '/media/'
 
 WAGTAIL_SITE_NAME = "SAGTA"
 
-PKCE_REQUIRED = False
-
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = 'https://sagta.org.za'
