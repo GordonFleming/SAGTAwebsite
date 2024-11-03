d09bee614d9deca0ea76c99a3966b56cc543911c
Gordon Fleming <flemingrgordon@gmail.com>
2024-11-03 23:21:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # Exit on error
 set -e
 
+# Ensure the static and media directories are writable
+chown -R wagtail:wagtail /usr/src/app/static
+chown -R wagtail:wagtail /usr/src/app/media
+
 # Run migrations and collect static files
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
