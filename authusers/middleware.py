29adb1656ab29b0491f886f28dc47bb0f98b94ff
Gordon Fleming <flemingrgordon@gmail.com>
2025-02-08 11:39:14 +0200

    update wagtail 6.4

--- authusers/middleware.py
+++ authusers/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mysite.settings.base import WAGTAIL_FRONTEND_LOGIN_URL
 
 class PermissionDeniedMiddleware:
     def __init__(self, get_response):
@@ -11,7 +12,7 @@ class PermissionDeniedMiddleware:
         # Check if it's a redirect to login page with a 'next' parameter
         if (response.status_code == 302
             and 
-            response.url.startswith('/accounts/login/') and 
+            response.url.startswith(WAGTAIL_FRONTEND_LOGIN_URL) and 
             request.user.is_authenticated):
             
             # Return a script that shows alert and redirects
