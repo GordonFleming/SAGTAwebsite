115fb95f4e0615f1bc5a8fa5cfeb45c649514679
Gordon Fleming <flemingrgordon@gmail.com>
2025-10-28 13:25:36 +0200

    Update views.py

--- authusers/views.py
+++ authusers/views.py
@@ -1,28 +1,15 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from django.http import HttpResponse
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.permissions import IsAdminUser
 from auth.adapter import validate_users
 
-# def index(request):
-#     result = validate_users()
-#     # You can use the result in your response
-#     response_data = {
-#         "message": "Validation task run!",
-#         "members_now_activated": result,
-#         "count": len(result),
-#     }
-
-#     return JsonResponse(response_data)
 
 class CustomSuccessView(APIView):
+    permission_classes = [IsAdminUser]
+
     def get(self, request):
         result = validate_users()
-        # You can use the result in your response
         response_data = {
             "message": "Validation task run!",
             "members": result,
