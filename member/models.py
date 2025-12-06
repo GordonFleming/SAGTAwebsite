4d19cbe2ee91024ca4bf736d97584cfa12225523
Gordon Fleming <flemingrgordon@gmail.com>
2025-12-06 15:47:42 +0200

    update pricing 2026

--- member/models.py
+++ member/models.py
@@ -1,14 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Validation
-TITLES = {
-    "mr": "Mr",
-    "mrs": "Mrs",
-    "miss": "Miss",
-    "ms": "Ms",
-    "none": "None"
-}
+TITLES = {"mr": "Mr", "mrs": "Mrs", "miss": "Miss", "ms": "Ms", "none": "None"}
 SADC_CHOICES = {
     "south_africa": "South Africa",
     "angola": "Angola",
@@ -25,42 +19,70 @@ SADC_CHOICES = {
     "seychelles": "Seychelles",
     "tanzania": "Tanzania",
     "zambia": "Zambia",
-    "zimbabwe": "Zimbabwe"
+    "zimbabwe": "Zimbabwe",
 }
 MEMBERSHIP_CHOICES = {
-    "individual": "Individual, SAGTA / AAG Joint Membership R520 / 1 year",
-    "retired_teachers": "Retired teachers, SAGTA / AAG Joint Membership R260 / 1 year",
-    "student_teachers": "Student teachers, SAGTA / AAG Joint Membership R260 / 1 year",
-    "corporate_institutional": "Corporate Institutional Membership (max. 5 staff members) R2370 / 1 year"
+    "individual": "Individual, SAGTA / AAG Joint Membership R600 / 1 year",
+    "retired_teachers": "Retired teachers, SAGTA / AAG Joint Membership R300 / 1 year",
+    "student_teachers": "Student teachers, SAGTA / AAG Joint Membership R300 / 1 year",
+    "corporate_institutional": "Corporate Institutional Membership (max. 5 staff members) R2560 / 1 year",
 }
 
 # Create your models here.
+
 
 class Member(models.Model):
     # One to one relationship with user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Members title
-    title = models.CharField(verbose_name='title', max_length=4, choices=TITLES, null=True, blank=False)
+    title = models.CharField(
+        verbose_name="title", max_length=4, choices=TITLES, null=True, blank=False
+    )
     # Name of your School/Tertiary Institution/Organisation
-    school = models.CharField(verbose_name='school', max_length=255, null=True, blank=False)
-    #Position/Job Title
-    position = models.CharField(verbose_name='position', max_length=255, null=True, blank=False)
+    school = models.CharField(
+        verbose_name="school", max_length=255, null=True, blank=False
+    )
+    # Position/Job Title
+    position = models.CharField(
+        verbose_name="position", max_length=255, null=True, blank=False
+    )
     # SADC Country of Residence
-    country = models.CharField(verbose_name='SADC Country', max_length=255, choices=SADC_CHOICES, null=True, blank=False)
+    country = models.CharField(
+        verbose_name="SADC Country",
+        max_length=255,
+        choices=SADC_CHOICES,
+        null=True,
+        blank=False,
+    )
     # secondary email, nullable
-    secondary_email = models.CharField(verbose_name='secondary_email', max_length=255, null=True, blank=True)
+    secondary_email = models.CharField(
+        verbose_name="secondary_email", max_length=255, null=True, blank=True
+    )
     # Cell of organisation
-    cell_org = models.CharField(verbose_name='cell', max_length=255, null=True, blank=False)
+    cell_org = models.CharField(
+        verbose_name="cell", max_length=255, null=True, blank=False
+    )
     # Personal cell
-    cell = models.CharField(verbose_name='cell', max_length=255, null=True, blank=True)
+    cell = models.CharField(verbose_name="cell", max_length=255, null=True, blank=True)
     # Website url of org
-    website = models.CharField(verbose_name='website', max_length=255, null=True, blank=True)
+    website = models.CharField(
+        verbose_name="website", max_length=255, null=True, blank=True
+    )
     # Postal Mailing Address of Organisation
-    address = models.CharField(verbose_name='address', max_length=255, null=True, blank=False)
+    address = models.CharField(
+        verbose_name="address", max_length=255, null=True, blank=False
+    )
     # SACE
-    sace = models.CharField(verbose_name='sace', max_length=255, null=True, blank=True)
+    sace = models.CharField(verbose_name="sace", max_length=255, null=True, blank=True)
     # Membership type
-    membership_type = models.CharField(verbose_name='membership_type', max_length=255, choices=MEMBERSHIP_CHOICES, null=True, blank=False)
+    membership_type = models.CharField(
+        verbose_name="membership_type",
+        max_length=255,
+        choices=MEMBERSHIP_CHOICES,
+        null=True,
+        blank=False,
+    )
+
 
 # Reference: https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#extending-the-existing-user-model
 # and https://docs.wagtail.org/en/stable/advanced_topics/customisation/custom_user_models.html#
--- payments/views.py
+++ payments/views.py
@@ -1,71 +1,80 @@
-from django.shortcuts import render, redirect
-from .models import Payment, UserWallet
-from django.db.models import Sum
-from member.models import Member
-from django.contrib.auth.models import Group
 import os
-from dotenv import load_dotenv
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.db.models import Sum
 from django.http import HttpResponseBadRequest
+from django.shortcuts import redirect, render
+from dotenv import load_dotenv
+
+from member.models import Member
+
+from .models import Payment, UserWallet
+
 load_dotenv()
+
 
 @login_required
 def initiate_payment(request):
-	if request.method == "POST":
-		membership_type = request.POST['membership_type']
-		email = request.POST['email']
-		
+    if request.method == "POST":
+        membership_type = request.POST["membership_type"]
+        email = request.POST["email"]
+
         # For student and retired
-		amount = 260
-	    # Logic for different payment amounts based on membership type
-		if membership_type == "individual":
-			amount = 520
-		elif membership_type == "corporate_institutional":
-			amount = 2370
-        
-		pk = os.environ.get('PAYSTACK_PUBLIC_KEY')
+        amount = 300
+        # Logic for different payment amounts based on membership type
+        if membership_type == "individual":
+            amount = 600
+        elif membership_type == "corporate_institutional":
+            amount = 2560
 
-		payment = Payment.objects.create(amount=amount, email=email, user=request.user)
-		payment.save()
+        pk = os.environ.get("PAYSTACK_PUBLIC_KEY")
 
-		context = {
-			'payment': payment,
-			'field_values': request.POST,
-			'paystack_pub_key': pk,
-			'amount_value': payment.amount_value(),
-            'membership_type': membership_type
-		}
-		return render(request, 'payments/make_payment.html', context)
+        payment = Payment.objects.create(amount=amount, email=email, user=request.user)
+        payment.save()
 
-	return render(request, 'payments/payment.html')
+        context = {
+            "payment": payment,
+            "field_values": request.POST,
+            "paystack_pub_key": pk,
+            "amount_value": payment.amount_value(),
+            "membership_type": membership_type,
+        }
+        return render(request, "payments/make_payment.html", context)
+
+    return render(request, "payments/payment.html")
+
 
 @login_required
 def verify_payment(request, ref, membership_type):
-	print(request)
-	payment = Payment.objects.get(ref=ref)
-	verified = payment.verify_payment()
-	user = request.user
-	if not membership_type:
-		return HttpResponseBadRequest("Membership type is missing.")
-	
-	if verified:
-		# Check if UserWallet exists for the user, create if it doesn't
-		user_wallet, created = UserWallet.objects.get_or_create(user=user)
+    print(request)
+    payment = Payment.objects.get(ref=ref)
+    verified = payment.verify_payment()
+    user = request.user
+    if not membership_type:
+        return HttpResponseBadRequest("Membership type is missing.")
 
-		total_amount = Payment.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
-		if user_wallet.balance < total_amount: 
-			user_wallet.balance += payment.amount
+    if verified:
+        # Check if UserWallet exists for the user, create if it doesn't
+        user_wallet, created = UserWallet.objects.get_or_create(user=user)
 
-		user_wallet.save()
-		member, created = Member.objects.get_or_create(user=user)
-		member.membership_type = membership_type
-		member.save()
-		
+        total_amount = (
+            Payment.objects.filter(user=user).aggregate(Sum("amount"))["amount__sum"]
+            or 0
+        )
+        if user_wallet.balance < total_amount:
+            user_wallet.balance += payment.amount
+
+        user_wallet.save()
+        member, created = Member.objects.get_or_create(user=user)
+        member.membership_type = membership_type
+        member.save()
+
         # Add the user to the members user group
-		group = Group.objects.get(name='Members')
-		user.groups.add(group)
-		user.save()
-	
-		print(request.user.username, " funded wallet successfully")
-		return render(request, "payments/success.html")
-	return render(request, "payments/success.html")
+        group = Group.objects.get(name="Members")
+        user.groups.add(group)
+        user.save()
+
+        print(request.user.username, " funded wallet successfully")
+        return render(request, "payments/success.html")
+    return render(request, "payments/success.html")
