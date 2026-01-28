52f105ecae1038757a1f19a4dba8a6cb058a4751
Gordon Fleming <flemingrgordon@gmail.com>
2026-01-28 20:54:26 +0200

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@
 # Exit on error
 set -eux
 
+echo "Setting up database directory..."
+mkdir -p db
+chmod -R a+rwX db
+
 echo "Restoring database from Litestream if available..."
-litestream restore -config litestream.yml -if-db-not-exists -if-replica-exists db.sqlite3
+litestream restore -if-db-not-exists -if-replica-exists db/site.sqlite3
 
 echo "Running migrations..."
 python manage.py migrate --noinput
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -160,7 +160,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'site.sqlite3'),
         'OPTIONS': {
             'transaction_mode': 'IMMEDIATE',
             'timeout': 5,  # seconds
