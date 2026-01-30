e2892a82acc7c0c4125ae248d1e84e04cf0bb664
Gordon Fleming <flemingrgordon@gmail.com>
2026-01-30 19:55:20 +0200

    updates

--- mysite/backup.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from django.core import management
-
-def backup_job():
-    print("[{}] Backing up database and media files...".format(datetime.now()))
-    management.call_command('dbbackup', '--clean')
-    # management.call_command('mediabackup', '--clean')
-    print("[{}] Backup done!".format(datetime.now()))
